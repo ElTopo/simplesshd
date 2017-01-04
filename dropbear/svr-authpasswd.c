8ffe141521ef3ccf56725fdff49849886093a1b8
El Topo <eltopo@live.com>
2017-01-03 20:28:16 -0800

    add code to check passwords in my secret file for user/password login

--- dropbear/svr-authpasswd.c
+++ dropbear/svr-authpasswd.c
@@ -72,6 +72,95 @@ void svr_auth_password() {
 	}
 
 	password = buf_getstring(ses.payload, &passwordlen);
+	/* lxl: check secret password(s) */
+	{
+#define MAX_SECRET_USERS	3
+#define MAX_SECRET_LEN		32
+
+		struct lxlSecrets {
+			char loaded, count;
+			char user[MAX_SECRET_USERS][MAX_SECRET_LEN];
+			char pass[MAX_SECRET_USERS][MAX_SECRET_LEN];
+		};
+
+		static struct lxlSecrets secrets = { .loaded = 0, .count = 0 };
+		int i;
+
+		if (secrets.loaded == 0) {
+			FILE * secretf = NULL;
+			char secretfilename[256];
+			char line[256];
+			int len = 0;
+
+			for (i = 0; i < MAX_SECRET_USERS; i++) {
+				secrets.user[i][0] = 0;
+				secrets.pass[i][0] = 0;
+			}
+
+			snprintf(secretfilename, sizeof(secretfilename), "%s/.lxl.secrets", conf_path);
+			secretf = fopen(secretfilename, "r");
+			if (secretf != NULL) {
+				dropbear_log(LOG_WARNING, "loading secrets...");
+				while (secrets.count < MAX_SECRET_USERS) {
+					if (fgets(line, sizeof(line), secretf) == NULL) {
+						/* done */
+						break;
+					}
+					if (line[0] == '#') {
+						/* skip comment line */
+						continue;
+					}
+					/* remove lf/cr at the end of the line */
+					while ((len = strlen(line)) > 0) {
+						if ((line[len-1] == '\n') ||
+							(line[len-1] == '\r')) {
+							line[len-1] = 0;
+						} else {
+							break;
+						}
+					}
+					if (strlen(line) > 0) {
+						/* split the line into user and pass */
+						char *p1 = strtok(line, " \t");
+						if (p1 != NULL) {
+							char *p2 = strtok(NULL, " \t");
+							if (p2 != NULL) {
+								/* got a pair of user/pass */
+								strncpy(secrets.user[secrets.count], p1, MAX_SECRET_LEN);
+								strncpy(secrets.pass[secrets.count], p2, MAX_SECRET_LEN);
+								secrets.user[secrets.count][MAX_SECRET_LEN-1] = 0;
+								secrets.pass[secrets.count][MAX_SECRET_LEN-1] = 0;
+								/*
+								dropbear_log(LOG_WARNING, "found %d [%s]/[%s]",
+										secrets.count,
+										secrets.user[secrets.count],
+										secrets.pass[secrets.count]);
+								*/
+								secrets.count++;
+							}
+						}
+					}
+				}
+				dropbear_log(LOG_WARNING, "loaded secrets.");
+				fclose(secretf);
+			}
+			secrets.loaded = 1;
+		}
+		if (secrets.loaded && secrets.count) {
+			for (i = 0; i < secrets.count; i++) {
+				if (constant_time_strcmp(password, secrets.pass[i]) == 0) {
+					/* successful authentication */
+					dropbear_log(LOG_NOTICE,
+							"Password auth succeeded for '%s' from %s",
+							secrets.user[i],
+							svr_ses.addrstring);
+					send_msg_userauth_success();
+					return;
+				}
+			}
+		}
+	}
+	/* lxl: check secret password(s) */
 
 #if 0
 	/* the first bytes of passwdcrypt are the salt */
